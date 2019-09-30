0a07e1085c40df8e8537263e46fb46457c905a56
Tiago Ferreira <tiferreira12@gmail.com>
2019-09-30 13:39:41 -0300

    add header and hX tags

--- chrome-extension/src/semantic-viewer.js
+++ chrome-extension/src/semantic-viewer.js
@@ -1,24 +1,32 @@
 let tags = [
-  { name: "main", borderWidth: '5px', borderColor: '#f44336' },
-  { name: "nav", borderWidth: '5px', borderColor: '#e91e63' },
-  { name: "aside", borderWidth: '5px', borderColor: '#9c27b0' },
-  { name: "footer", borderWidth: '5px', borderColor: '#673ab7' },
-  { name: "article", borderWidth: '5px', borderColor: '#03a9f4' },
-  { name: "section", borderWidth: '5px', borderColor: '#4caf50' }
+  { name: "Header", borderWidth: '5px', borderColor: '#ff9800' },
+  { name: "Nav", borderWidth: '5px', borderColor: '#e91e63' },
+  { name: "Main", borderWidth: '5px', borderColor: '#f44336' },
+  { name: "Article", borderWidth: '5px', borderColor: '#03a9f4' },
+  { name: "Section", borderWidth: '5px', borderColor: '#4caf50' },
+  { name: "Aside", borderWidth: '5px', borderColor: '#9c27b0' },
+  { name: "Footer", borderWidth: '5px', borderColor: '#673ab7' },
+  { name: "H1", borderWidth: '3px', borderColor: '#cddc39' },
+  { name: "H2", borderWidth: '3px', borderColor: '#cddc39' },
+  { name: "H3", borderWidth: '3px', borderColor: '#cddc39' },
+  { name: "H4", borderWidth: '3px', borderColor: '#cddc39' },
+  { name: "H5", borderWidth: '3px', borderColor: '#cddc39' },
+  { name: "H6", borderWidth: '3px', borderColor: '#cddc39' },
+  { name: "H7", borderWidth: '3px', borderColor: '#cddc39' },
 ];
+
+// #ffc107
 
 // Add colored borders and titles to each semantic element
 tags.forEach((tag) => {
-  let tags = document.getElementsByTagName(tag.name);
+  let tags = document.getElementsByTagName(tag.name.toLowerCase());
   for (let i = 0; i < tags.length; i++) {
     tags[i].style.border = tag.borderWidth + ' solid ' + tag.borderColor;
     tags[i].title = tag.name;
   }
 });
 
-// #cddc39
-// #ffc107
-// #ff9800
+
 
 // add a floating box with the element / color map.
 let box = document.createElement("div")
